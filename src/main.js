06d3932637e8b15060e04c5a02f4edbe072ef0bb
Yaroslav Orlov <159253787+YaroslavOrlov17@users.noreply.github.com>
2024-07-23 09:19:07 +0300

    fetch add

--- src/main.js
+++ src/main.js
@@ -10,6 +10,21 @@ function handleSubmit(event) {
     console.log('Заполните форму!');
   }
 
+  // Запрос на сервер -----------------------
+
+  const searchParams = new URLSearchParams({
+    key: '16437496-6d56e114a2cb1088922bcb1d6',
+    q: inputValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  fetch(`https://pixabay.com/api/?${searchParams}`)
+    .then(response => response.json())
+    .then(images => console.log(images))
+    .catch(error => console.log(error));
+
   form.reset();
 }
 
